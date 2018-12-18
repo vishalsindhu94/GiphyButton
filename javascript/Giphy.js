03529d0d7d60f2f3dcf054aa2d1c51c41eace362
unknown <vishalsindhu@aim.com>
2018-12-18 14:41:32 -0500

    error

--- javascript/Giphy.js
+++ javascript/Giphy.js
@@ -1,57 +1,63 @@
 
     var topics= ["dog","cat","mouse","rat","bear","bird"];
-    var animal;
-    function displayAnimalInfo() {
+    var image="";
+    function renderButtons(){
+      $("#createdButtons").empty();
+      $("#giphySearch").val("");
 
-         animal = $(this).attr("data-name");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=JiKCiDrv0cfw357iDe8Bd74lbNMXfHWd&q=animal&limit=10&offset=0&rating=PG-13&lang=en"
+      for (var i = 0; i < topics.length; i++) {
+        var newButton = $("<button class='btn btn-primary'>");
+          newButton.addClass("animal-btn");
+          newButton.attr("data-animal", topics[i]);
+          newButton.text(topics[i]);
+        $("#createdButtons").append(newButton);
+        $("#createdButtons").append("");
+      }
+    }
+    renderButtons();
+    displayAnimalInfo();
+
+    $("#search").on("click", function(event) {
+      event.preventDefault();
+      animalInput = $("#giphySearch").val().trim();
+      animal= $(this).attr("data-animal");
+      console.log(animal);
+      topics.push(animalInput);
+      renderButtons();  
+      return false;
+    });
+
+    $(document).on("click",".animal-btn",displayAnimalInfo)
+    function displayAnimalInfo() {
+    var animal = $(this).attr("data-animal");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=JiKCiDrv0cfw357iDe8Bd74lbNMXfHWd&limit=10&lang=en"
         console.log(queryURL);
-   
+        console.log(animal);
+        console.log(topics);
+        $("#giphyOutput").empty();
     $.ajax({
       url: queryURL,
       method: "GET"
     }).then(function(response) {
-
         console.log(response);
         console.log("hello");
+        console.log(topics);
+        renderButtons();
+        var results = response.data;
         $("#giphyOutput").empty();
-        var results=response.data;
         for(var i = 0; i < results.length; i++)
-            var giphyDiv= $("<div>");
-            $(giphyDiv).addClass("giphyDiv");
-            var rating = response.rating;
-            var displayRating = $("<p>").text("Rating: " + rating);
-            $(giphyDiv).append(displayRating);
-
-            var imgURL = results[i].images;
-            var image = $("<img>").attr("src", imgURL);
-            $(giphyDiv).addClass("image");
-            $(giphyDiv).append(image);
-            $("#giphyOutput").prepend(giphyDiv);
+            var animalDiv = $("<div>");
+            var displayRating = $("<p>").text("Rating: " + results[i].rating);
+            console.log(animal);
+            console.log(displayRating);
+            var image = $("<img>");
+            image.attr("src", results[i].images.fixed_height.url);
+            console.log(image);
+            animalDiv.append(displayRating);
+            animalDiv.append(image);
+            $("#giphyOutput").append(animalDiv);
+            
     });
 };
-    function renderButtons(){
-        $("#createdButtons").empty();
-        $("#giphySearch").val("");
-
-        for (var i = 0; i < topics.length; i++) {
-          var newButton = $("<button>");
-            newButton.addClass("animal-btn");
-            newButton.attr("data-name", topics[i]);
-            newButton.text(topics[i]);
-          $("#createdButtons").append(newButton);
-        }
-      }
-      renderButtons();
-
-      $("#search").on("click", function(event) {
-        event.preventDefault();
-        animal = $("#giphySearch").val().trim();
-        topics.push(animal);
-      });
-
-      $(document).on("click", ".animal-btn", displayAnimalInfo);
-      displayAnimalInfo();
-      
     
 
