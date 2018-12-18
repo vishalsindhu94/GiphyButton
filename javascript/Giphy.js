727c2c4fe2aa9ec103e4bf18b0202bb6d2bd2365
unknown <vishalsindhu@aim.com>
2018-12-17 22:23:32 -0500

    few more changes

--- javascript/Giphy.js
+++ javascript/Giphy.js
@@ -1,10 +1,10 @@
 
-    var animals= ["dog","cat","mouse","rat","bear","bird"];
+    var topics= ["dog","cat","mouse","rat","bear","bird"];
     var animal;
     function displayAnimalInfo() {
 
          animal = $(this).attr("data-name");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=JiKCiDrv0cfw357iDe8Bd74lbNMXfHWd&q="+animal+"&limit=10&offset=0&rating=PG-13&lang=en";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=JiKCiDrv0cfw357iDe8Bd74lbNMXfHWd&q=animal&limit=10&offset=0&rating=PG-13&lang=en"
         console.log(queryURL);
    
     $.ajax({
@@ -17,38 +17,41 @@
         $("#giphyOutput").empty();
         var results=response.data;
         for(var i = 0; i < results.length; i++)
+            var giphyDiv= $("<div>");
+            $(giphyDiv).addClass("giphyDiv");
+            var rating = response.rating;
+            var displayRating = $("<p>").text("Rating: " + rating);
+            $(giphyDiv).append(displayRating);
 
-        var rating = response.rating;
-        var displayRating = $("<p>").text("Rating: " + rating);
-        animalDiv.append(displayRating);
-
-        var imgURL = response.images;
-        var image = $("<img>").attr("src", imgURL);
-        animalDiv.append(image);
-        $("#giphyOutput").prepend(animalDiv);
+            var imgURL = results[i].images;
+            var image = $("<img>").attr("src", imgURL);
+            $(giphyDiv).addClass("image");
+            $(giphyDiv).append(image);
+            $("#giphyOutput").prepend(giphyDiv);
     });
 };
     function renderButtons(){
         $("#createdButtons").empty();
+        $("#giphySearch").val("");
 
-        for (var i = 0; i < animals.length; i++) {
+        for (var i = 0; i < topics.length; i++) {
           var newButton = $("<button>");
-          
             newButton.addClass("animal-btn");
-            newButton.attr("data-name", animals[i]);
-            newButton.text(animals[i]);
+            newButton.attr("data-name", topics[i]);
+            newButton.text(topics[i]);
           $("#createdButtons").append(newButton);
         }
       }
+      renderButtons();
+
       $("#search").on("click", function(event) {
         event.preventDefault();
         animal = $("#giphySearch").val().trim();
-        animals.push(animal);
-        renderButtons();
+        topics.push(animal);
       });
 
       $(document).on("click", ".animal-btn", displayAnimalInfo);
-      renderButtons();
       displayAnimalInfo();
+      
     
 
